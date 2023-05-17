c88697e7239e5b80e27556ea2fb1464ea8c07712
Shakeel Khuhro <shakeelahmedkhuhro5454@gmail.com>
2023-05-17 09:42:25 +0500

    js updated

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 const getColor = () => {
-    // hex code (hexa decimal code)
     const randomNumber = Math.floor(Math.random() * 16777215);
-    const randomCode = "#" + randomNumber.toString(16); // it will convert to hexadecimal code because 16 is used for hexadecimal
-    console.log(randomNumber, randomCode)
-    // console.log(randomCode)
+    const randomCode = "#" + randomNumber.toString(16);
+    console.log(randomNumber, randomCode);
 
-    document.getElementById('color-id').innerText = randomCode;
+    document.getElementById('color-code').innerText = randomCode;
     document.body.style.backgroundColor = randomCode;
 
-    navigator.clipboard.writeText(randomCode)
+    navigator.clipboard.writeText(randomCode);
 }
 
-//  event color
 document.getElementById('btn').addEventListener(
     "click",
-    getColor // don't use getColor() i-e don't use brackets
-)
+    getColor
+);
 
-//  initial call when we load page first time
 getColor();
